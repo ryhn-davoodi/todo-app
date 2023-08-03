b15337a37015b830e174e7634764dbf744d294ac
Reyhaneh <reyhaneh.gh.davodi@gmail.com>
2023-08-03 14:26:18 +0330

    commit edit button

--- gui.py
+++ gui.py
@@ -5,11 +5,17 @@ import PySimpleGUI as sg
 lable=sg.Text('enter a todo:')
 input_Box=sg.InputText(key="todo")
 add_button=sg.Button("Add")
-window=sg.Window('to do app',layout=[[lable],[input_Box,add_button]],
-                 font=['tahome',20])
+edit_button=sg.Button("Edit")
+list_box=sg.Listbox(values=function.get_todos(),key="todos",
+                    enable_events=True,
+                    size=[45,10])
+window=sg.Window('to do app',layout=[[lable],
+               [input_Box,add_button],
+               [list_box,edit_button]],
+               font=['tahome',20])
 while True:
     event,value=window.read()
-    print(type(event))
+    print(event)
     print(value)
     match event:
         case 'Add':
@@ -17,5 +23,19 @@ while True:
             new_todo=value['todo'] +"\n"
             todos.append(new_todo)
             function.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            new_todo=value['todo'] +'\n'
+            todo_to_edit=value['todos'][0]
+            todos=function.get_todos()
+            index=todos.index(todo_to_edit)
+            todos[index]=new_todo
+            function.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "todos":
+            window['todo'].update(value=value['todos'][0])
+        case sg.WINDOW_CLOSED:
+            break
 window.close()
 
